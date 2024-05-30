9bf18ce20080db1548bf59a74cbb4e4a2dbc25cd
Tarmo Pikaro <tapika@yahoo.com>
2024-05-30 18:52:12 +0300

    fix enumeration

--- src/Conductor.AzureDevOps.Api/Models/GitPullRequestMergeStrategy.cs
+++ src/Conductor.AzureDevOps.Api/Models/GitPullRequestMergeStrategy.cs
@@ -1,10 +1,13 @@
 ﻿namespace Conductor.AzureDevOps.Api.Models
 {
+	/// <summary>
+	/// See https://docs.microsoft.com/en-us/javascript/api/azure-devops-extension-api/gitpullrequestmergestrategy
+	/// </summary>
 	public enum GitPullRequestMergeStrategy
 	{
-		NoFastForward,
-		Rebase,
-		RebaseMerge,
-		Squash
+		NoFastForward = 1,
+		Squash = 2,
+		Rebase = 3,
+		RebaseMerge = 4
 	}
 }
